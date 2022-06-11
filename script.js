9e7b6b50d1befe93c8224c9a57e195c25388232a
Elmar099 <elmar6999@gmail.com>
2022-06-10 21:09:21 -0700

    added hex code

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let dis = 0
     btn.addEventListener('click', function(){
         dis += 1
         count.innerHTML = dis;
-        const randomColor = Math.ceil(Math.random()*16777215).toString(16);
+        let randomColor = document.getElementById("hex").innerHTML =  Math.ceil(Math.random()*16777215).toString(16);
         document.body.style.backgroundColor = "#" + randomColor;
-        color.innerHTML = "#" + randomColor; 
     })
+    
