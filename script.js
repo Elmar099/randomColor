83481a47a465e2189f405834799b703a7764226e
Elmar099 <elmar6999@gmail.com>
2022-08-02 23:15:14 -0700

    fixed changes

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let btn = document.getElementById('change')
 let count = document.getElementById('count')
+let copy = document.getElementById('copy')
 let dis = 0
-    btn.addEventListener('click', function(){
+
+btn.addEventListener('click', function(){
         dis += 1
         count.innerHTML = dis;
         let randomColor = document.getElementById("hex").innerHTML =  Math.ceil(Math.random()*16777215).toString(16);
         document.body.style.backgroundColor = "#" + randomColor;
     })
-    
